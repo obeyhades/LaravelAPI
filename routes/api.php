a13a12e8184f0282f522f2edd8c051ba6940142b
obeyhades <abdu.nazar145@gmail.com>
2025-02-05 21:30:18 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+route::post('/register',[AuthController::class, 'register']);
+route::post('/login',[Authcontroller::class, 'login']);
+route::get('/logout',[Authcontroller::class, 'logout'])->middleware('auth:sanctum');
+
+
+
+
+Route::post('/test', function (Request $request) {
+    $data = $request->test;
+
+    dump($data);
+
+    return response()->json(['Message'=> "Incoming date was: $data"]);
+});
+
+Route::get('/getRouteTest', function (Request $request){
+    $request->validate([
+        'test' => 'required | string',
+    ]);
+    try {
+        if(true) {
+            throw new Exception("There will be an error");
+        }
+
+        return response()->json(['message'=> "hello from api"]);
+    } catch (\Exception $e) {
+        return response()->json(['message'=> "Internal Server Error"], 500);
+    }
+}); 
